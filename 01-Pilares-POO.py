c42f8dd29b7f8ac418a0bc19c083e4d6774a99b8
Matias Alessandrello <matialess@gmail.com>
2025-05-01 18:57:24 -0300

    Pilares de POO

---
+++ 01-Pilares-POO.py
@@ -0,0 +1,20 @@
+class Persona:
+    def __init__(self,nombre,edad):
+        self.__nombre = nombre
+        self.__edad = edad
+    
+    def get_nombre(self):
+        return self.__nombre
+    
+    def set_nombre(self, nuevo_nombre):
+        self.__nombre = nuevo_nombre
+    
+    def get_edad(self):
+        return self.__edad
+    
+persona1 = Persona("Ana", 30)
+print(persona1.get_nombre())
+nuevo_nombre = input("Ingrese el nuevo nombre: ")
+persona1.set_nombre(nuevo_nombre)
+print(persona1.get_nombre())
+print(persona1.get_edad())
---
+++ 02-Pilares-POO.py
@@ -0,0 +1,11 @@
+class Coche:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+    def acelerar(self):
+        print(f"{self.marca} {self.modelo} esta acelerando")
+
+
+mi_coche = Coche("Fiat", "Mobi")
+mi_coche.acelerar()
